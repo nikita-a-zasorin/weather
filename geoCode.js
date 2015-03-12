5ba3f51d8aaf94ea67a03d14db767d1359da5e98
Noah Keller <nkeller@mobiquityinc.com>
2015-03-12 14:30:14 -0400

    Noah's modifications

--- geoCode.js
+++ geoCode.js
@@ -1,45 +1,68 @@
-            'use strict';
+'use strict';
 
-            angular.module('weatherApp', ['weatherDirectives']);
+angular.module('weatherApp', []);
 
+/**
+ * This service stores the data relating to the weather, so it can be shared between googlePlacesController and searchForm
+ */
+angular.module('weatherApp').factory('weather', ['$http',
+    function ($http) {
+        var weather = '';
+        var place = {};
 
-            function searchForm($http){
-                this.location = '';
+        return {
+            weather: weather,
+            place: place
+        };
+    }
+]);
 
-                this.doSearch = function(){
-                    if(this.location === ''){
-                        alert('Not found...');
-                    } else {
-                        var url = "https://api.forecast.io/forecast/7ce90a9cbabf8e10728551e92b826887/"+this.location+"?callback=JSON_CALLBACK";
+angular.module('weatherApp').controller('searchForm', ['$http', 'weather',
+    function ($http, weather) {
+        var self = this;
 
+        self.place = weather.place;
+        self.weather = weather.weather;
+        self.coordinates = '';
+        self.locName = weather.place.formatted_address;
 
-                        $http.jsonp(url).success(function(data) {
-                        this.title = "Weather for "+ this.location + "...";
-                        this.weather = "Temperature is: " + data.currently.temperature + " degrees Fahrenheit.";
-                        this.location = '';
-                    });
+        self.doSearch = function () {
+            if (!self.place) {
+                alert('Not found...');
+            } else {
+                var url = "https://api.forecast.io/forecast/7ce90a9cbabf8e10728551e92b826887/" + self.place.geometry.location.k + "," + self.place.geometry.location.D + "?callback=JSON_CALLBACK";
 
-                        this.coordinates = "Weather for: " + this.location;
-                    }
-                };
-            }
-
-
-            angular.module('weatherDirectives', []).
-                directive('googlePlaces', function(){
-                    return {
-                        restrict:'E',
-                        replace:true,
-                        // transclude:true,
-                        scope: {location:'='},
-                        template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
-                        link: function($scope, elm, attrs){
-                        var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
-                        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                        var place = autocomplete.getPlace();
-                        $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
-                        $scope.$apply();
-                            });
-                        }
-                    }
+                $http.jsonp(url).success(function (data) {
+                    console.log('Got some data!');
+                    console.log(data);
+                    self.title = "Weather for " + self.place.formatted_address + "...";
+                    self.weather = "Temperature is: " + data.currently.temperature + " degrees Fahrenheit.";
                 });
+
+                self.coordinates = "Weather for: " + self.location;
+            }
+        };
+    }
+]);
+
+angular.module('weatherApp').directive('googlePlaces', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        controller: 'googlePlacesController', // no need for 'controller as' syntax here, as we access it from ctrl argument
+        template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
+        link: function ($scope, elm, attrs, ctrl) {
+            var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                ctrl.setPlace(place);
+            });
+        }
+    }
+}).controller('googlePlacesController', ['weather',
+    function (weather) {
+        this.setPlace = function (place) {
+            angular.extend(weather.place, place);
+        };
+    }
+]);
